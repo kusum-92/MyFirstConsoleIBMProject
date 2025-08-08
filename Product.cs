1abea76741280a5f7219d0f2c1ed9624d92aaee8
kusum <gkusum922@gmail.com>
2025-08-08 09:52:24 +0530

    product class added

---
+++ Product.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleIBMProject
+{
+    internal class Product //only two type of classes - internal and public
+    {
+        //data members of Product class
+        private string productName;
+        private double productPrice;
+        private int productQuantity;//instance variable
+
+        public Product()  //create instance of Product class - default constructor 
+        {
+
+        }
+        public Product(string name,double price, int quantity) //parameterized constructor
+        {
+            productName = name;
+            productPrice = price;
+            productQuantity = quantity;
+        }
+        //properties
+        public string ProductName //should start from upper case letter
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        public double ProductPrice
+        {
+            get { return productPrice; }
+            set { productPrice = value; }
+        }
+        public int ProductQuantity
+        {
+            get { return productQuantity; }
+            set { productQuantity = value; }
+        }
+        //method to display product details
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine($"Product Name: {productName}");
+            Console.WriteLine($"Product Price: {productPrice}");
+            Console.WriteLine($"Product Quantity: {productQuantity}");
+        }
+        //method to calculate total price of the product
+        public double CalculateTotalPrice()
+        {
+            return productPrice * productQuantity;
+        }
+        //method to apply discount on the product
+
+        public double ApplyDiscount(double discountPercentage)
+        {
+            double discountAmount = (productPrice * discountPercentage) / 100;
+            return productPrice - discountAmount;
+        }
+        //method to check if the product is in stock
+        public bool IsInStock()
+        {
+            return productQuantity > 0;
+        }
+        //method to restock the product
+        public void Restock(int additionalquantity)
+        {
+            if (additionalquantity > 0)
+            {
+                productQuantity += additionalquantity;
+                Console.WriteLine($"{additionalquantity} units added to stock. New quantity: {productQuantity}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid quantity to restock.");
+            }
+        }
+        //method to update product price    
+        public void UpdatePrice(double newPrice)
+        {
+            if (newPrice > 0)
+            {
+                productPrice = newPrice;
+                Console.WriteLine($"Product price updated to: {productPrice}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid price. Price must be greater than zero.");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -89,3 +89,19 @@ for (int i=0; i < 5; i++)
 double avg= (total / 5);
 Console.WriteLine($"total marks {total}");
 Console.WriteLine($"Average marks {avg}");
+
+int[,] twoDArays = new int[3, 3]
+{
+    {1,2,3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+
+for (int i = 0; i < twoDArays.GetLength(0); i++)
+{
+    for (int j = 0; j < twoDArays.GetLength(1); j++)
+    {
+        Console.Write(twoDArays[i, j] + " ");
+    }
+    Console.WriteLine();
+}
